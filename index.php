d16a7dc05f3d8b8afb1b8f30010537871cb54152
codystack <ebuzzadvert@gmail.com>
2020-05-11 17:26:12 +0100

    advert image added

--- index.php
+++ index.php
@@ -24,211 +24,30 @@
     </div>
     <!-- slider_area_end -->
 
-    <!-- service_area_start  -->
-    <div class="service_area">
+    <div class="about_area plus_padding">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="section_title text-center mb-90">
-                        <span class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".1s"></span>
-                        <h3 class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".2s">What we offer for you</h3>
-                        <p class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">We provide online instant cash loans with quick approval that suit your term</p>
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-md-6">
+                    <div class="about_img wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".3s">
+                        <img src="img/hmeimg.jpg" alt="">
                     </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-4 col-md-6">
-                    <div class="single_service wow fadeInLeft" data-wow-duration="1.2s" data-wow-delay=".5s">
-                        <div class="service_icon_wrap text-center">
-                            <div class="service_icon ">
-                                <img src="img/svg_icon/service_1.png" alt="">
-                            </div>
-                        </div>
-                        <div class="info text-center">
-                            <span>Home Loan</span>
-                            <h3>$3000-$10000</h3>
-                        </div>
-                        <div class="service_content">
-                            <ul>
-                                <li> Borrow - $350 over 3 months </li>
-                                <li> Interest rate - 292% pa fixed</li>
-                                <li>Total amount payable - $525.12</li>
-                                <li>Representative - 1,286% APR</li>
-                            </ul>
-                            <div class="apply_btn">
-                                <button class="boxed-btn3" type="submit">Apply Now</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="single_service wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">
-                        <div class="service_icon_wrap text-center">
-                            <div class="service_icon ">
-                                <img src="img/svg_icon/service_2.png" alt="">
-                            </div>
-                        </div>
-                        <div class="info text-center">
-                            <span>car Loan</span>
-                            <h3>$3000-$10000</h3>
-                        </div>
-                        <div class="service_content">
-                            <ul>
-                                <li> Borrow - $350 over 3 months </li>
-                                <li> Interest rate - 292% pa fixed</li>
-                                <li>Total amount payable - $525.12</li>
-                                <li>Representative - 1,286% APR</li>
-                            </ul>
-                            <div class="apply_btn">
-                                <button class="boxed-btn3" type="submit">Apply Now</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="single_service wow fadeInRight" data-wow-duration="1.2s" data-wow-delay=".5s">
-                        <div class="service_icon_wrap text-center">
-                            <div class="service_icon ">
-                                <img src="img/svg_icon/service_3.png" alt="">
-                            </div>
-                        </div>
-                        <div class="info text-center">
-                            <span>Education Loan</span>
-                            <h3>$3000-$10000</h3>
-                        </div>
-                        <div class="service_content">
-                            <ul>
-                                <li> Borrow - $350 over 3 months </li>
-                                <li> Interest rate - 292% pa fixed</li>
-                                <li>Total amount payable - $525.12</li>
-                                <li>Representative - 1,286% APR</li>
-                            </ul>
-                            <div class="apply_btn">
-                                <button class="boxed-btn3" type="submit">Apply Now</button>
-                            </div>
-                        </div>
+                <div class="col-lg-6 col-md-6">
+                    <div class="about_img wow fadeInLeft" data-wow-duration="1s" data-wow-delay=".3s">
+                        <img src="img/hmeimg1.jpg" alt="">
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- service_area_end  -->
     
 <?php require('./components/aboutarea.php');?>
 
-    <div class="works_area">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="section_title text-center mb-90">
-                        <span class="wow lightSpeedIn" data-wow-duration="1s" data-wow-delay=".1s"></span>
-                        <h3 class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".2s">How it Works</h3>
-                        <p class="wow fadeInUp" data-wow-duration="1s" data-wow-delay=".3s">We provide online instant cash loans with quick approval that suit your term</p>
+                    <div class="wowt" data-wow-duration="1s" data-wow-delay=".3s">
+                        <img src="img/hmeslide.jpg" alt="" class="homeadvert">
                     </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6 col-lg-4">
-                    <div class="single_works wow fadeInUp" data-wow-duration="1s" data-wow-delay=".4s">
-                        <span>
-                            01
-                        </span>
-                        <h3>Apply for loan</h3>
-                        <p>We will customize a loan based on the 
-                            amount of cash your company need term</p>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-4">
-                    <div class="single_works wow fadeInUp" data-wow-duration="1s" data-wow-delay=".5s">
-                        <span>
-                            02
-                        </span>
-                        <h3>Application review</h3>
-                        <p>We will customize a loan based on the 
-                            amount of cash your company need term</p>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-4">
-                    <div class="single_works wow fadeInUp" data-wow-duration="1s" data-wow-delay=".6s">
-                        <span>
-                            03
-                        </span>
-                        <h3>Get funding fast</h3>
-                        <p>We will customize a loan based on the 
-                            amount of cash your company need term</p>
-                    </div>
-                </div>
-            </div>
         </div>
-    </div>
-
-    <div class="accordion_area">
-        <div class="container">
-            <div class="row justify-content-end">
-                <div class="col-lg-6">
-                    <div class="faq_ask pl-68">
-                        <h3 class="wow fadeInRight" data-wow-duration="1s" data-wow-delay=".3s">Frequently ask</h3>
-                            <div id="accordion">
-                                <div class="card wow fadeInUp" data-wow-duration="1.1s" data-wow-delay=".3s">
-                                    <div class="card-header" id="headingOnee">
-                                      <h5 class="mb-0">
-                                        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOnee" aria-expanded="true" aria-controls="collapseOnee">
-                                            Adieus who direct esteem It esteems luckily?
-                                        </button>
-                                      </h5>
-                                    </div>
-                                
-                                    <div id="collapseOnee" class="collapse show" aria-labelledby="headingOnee" data-parent="#accordion">
-                                      <div class="card-body">
-                                        Esteem spirit temper too say adieus who direct esteem esteems luckily or picture placing drawing.
-                                      </div>
-                                    </div>
-                                  </div>
-                                    <div class="card wow fadeInUp" data-wow-duration="1.2s" data-wow-delay=".4s">
-                                        <div class="card-header" id="headingOne">
-                                            <h5 class="mb-0">
-                                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                                                    Who direct esteem It esteems?
-                                                </button>
-                                            </h5>
-                                        </div>
-                                        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion" style="">
-                                            <div class="card-body">Esteem spirit temper too say adieus who direct esteem esteems luckily or picture placing drawing.
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="card wow fadeInUp" data-wow-duration="1.3s" data-wow-delay=".5s">
-                                        <div class="card-header" id="headingThree">
-                                            <h5 class="mb-0">
-                                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                                    Duis consectetur feugiat auctor?
-                                                </button>
-                                            </h5>
-                                        </div>
-                                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion" style="">
-                                            <div class="card-body">Esteem spirit temper too say adieus who direct esteem esteems luckily or picture placing drawing.
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="card wow fadeInUp" data-wow-duration="1.4s" data-wow-delay=".6s">
-                                        <div class="card-header" id="headingThree4">
-                                            <h5 class="mb-0">
-                                                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree4" aria-expanded="false" aria-controls="collapseThree4">
-                                                    Consectetur feugiat auctor?
-                                                </button>
-                                            </h5>
-                                        </div>
-                                        <div id="collapseThree4" class="collapse" aria-labelledby="headingThree4" data-parent="#accordion" style="">
-                                            <div class="card-body">Esteem spirit temper too say adieus who direct esteem esteems luckily or picture placing drawing.
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
 <?php require('./components/testimonials.php');?>
 
